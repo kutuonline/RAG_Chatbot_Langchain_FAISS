03f2a69ead41f94addefcb6271d2d32ea1cc1a47
Andriansyah Doni <andrean.devz@gmail.com>
2025-08-17 08:57:46 +0700

    Update rag_chatbot.py

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -14,8 +14,8 @@ from langchain_community.chat_models import ChatOpenAI
 # Load environment variables from .env file
 load_dotenv()
 
-os.environ["API_KEY"] = "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"
-GROQ_API_KEY = os.getenv("API_KEY")
+# os.environ["GROQ_API_KEY"] = "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"
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 MODEL = 'llama-3.3-70b-versatile'
 llm = ChatGroq(
