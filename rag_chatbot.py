72186606be2688b01ca04d52ea9badb38bd7969f
Andriansyah Doni <andrean.devz@gmail.com>
2026-04-28 11:11:02 +0700

    Update rag_chatbot.py

--- rag_chatbot.py
+++ rag_chatbot.py
@@ -9,7 +9,8 @@ from langchain_groq import ChatGroq
 from groq import Groq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains.question_answering import load_qa_chain
+# from langchain.chains.question_answering import load_qa_chain
+from langchain_classic.chains.question_answering import load_qa_chain
 from langchain_community.chat_models import ChatOpenAI
 
 # Load environment variables from .env file
